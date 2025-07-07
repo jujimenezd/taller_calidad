5edf8f6a89f1f39fe3965660abd3420c4c50b524
jujimenezd <jujimenezd@unal.edu.co>
2025-07-07 01:56:34 -0500

    first problem

---
+++ validate_username/main.py
@@ -0,0 +1,28 @@
+from string import ascii_lowercase
+
+
+def is_valid_username(username: str) -> bool:
+    username = username.lower()
+    punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^`{|}~"""
+    for i in username:
+        if i in punctuation:
+            print(i)
+            return False
+
+    if username == "":
+        return False
+    elif len(username) < 6:
+        return False
+    elif len(username) > 12:
+        return False
+
+    if username[0] not in ascii_lowercase:
+        return False
+    else:
+        return True
+
+    # for i in username[0]:
+    #     if i not in ascii_lowercase:
+    #         return f"{i} no pasa"
+    #     else:
+    #         return username
---
+++ validate_username/tests/test_main.py
@@ -0,0 +1,35 @@
+import pytest
+from validate_username.main import is_valid_username
+
+
+@pytest.mark.parametrize(
+    "username, expected",
+    [
+        ("juanes34", True),
+        ("3uanes34", False),
+        ("", False),
+        ("jujimenezd_23", False),
+        ("000000000", False),
+        ("juanes34", True),
+        ("1", False),
+        ("$$#jujimenezd", False),
+        ("abc", False),
+        ("}{}43juan", False),
+    ],
+)
+def test_validate_username_params(username, expected):
+    assert is_valid_username(username) == expected
+
+
+# def test_validate_username():
+#     assert is_valid_username("juanes34") == True
+#     assert is_valid_username("3juan34") == False
+#     assert is_valid_username("") == False
+#     assert is_valid_username("jujimenezd_23") == False
+#     assert is_valid_username("jujimenezd_") == True
+#     assert is_valid_username("000000000") == False
+#     assert is_valid_username("1") == False
+#     assert is_valid_username("A") == False
+#     assert is_valid_username("$$#jujimenezd") == False
+#     assert is_valid_username("DUNKELHEIT") == True
+#     assert is_valid_username("Tragamilk") == True
